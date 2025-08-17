a53f6d5ebeec85b6556031f5f98c2d8a6a9d437e
prusov-code <prusov-official@yandex.ru>
2025-08-18 01:43:36 +0300

    Add Prime Number game

---
+++ src/PrimeGame.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace BrainGames\PrimeGame;
+
+use function BrainGames\Cli\playGame;
+
+function startPrimeGame(): void
+{
+    $questions = [];
+    $answers = [];
+    for ($i = 0; $i < 3; $i++) {
+        $questions[$i] = rand(1, 100);
+        $answers[$i] = isPrime($questions[$i]) ? 'yes' : 'no';
+    }
+    playGame($questions, $answers, 'Answer "yes" if given number is prime. Otherwise answer "no".');
+}
+
+function isPrime(int $number): bool
+{
+    if ($number < 2) {
+        return false;
+    }
+    if ($number === 2) {
+        return true;
+    }
+    if ($number % 2 === 0) {
+        return false;
+    }
+    for ($i = 3; $i < sqrt($number); $i += 2) {
+        if ($number % $i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
