a586d318b7afa2fb24043df1a14388f76a439eaa
prusov-code <prusov-official@yandex.ru>
2025-08-18 02:09:47 +0300

    Fix minor src/Cli.php, PHPStan warnings fixed

--- src/Cli.php
+++ src/Cli.php
@@ -2,8 +2,8 @@
 
 namespace BrainGames\Cli;
 
-use function Cli\line;
-use function Cli\prompt;
+use function cli\line;
+use function cli\prompt;
 
 function sayGreeting(string $gameDescription = ''): string
 {
@@ -21,7 +21,7 @@ function sayCongratulions(string $userName): void
     line("Congratulations, $userName!");
 }
 
-function sayUserLose(string $userAnswer, string $correctAnswer, string $userName): void
+function sayUserLose(mixed $userAnswer, string $correctAnswer, string $userName): void
 {
     line("'$userAnswer' is wrong answer ;(. Correct answer was '$correctAnswer'.");
     line("Let's try again, $userName!");
