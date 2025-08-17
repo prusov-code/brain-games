da61e257d362211dd7fecc05bea6b614b6173896
prusov-code <prusov-official@yandex.ru>
2025-08-17 20:52:24 +0300

    Add src/Cli.php and add it to autoload in composer.json

---
+++ src/Cli.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BrainGames\Cli;
+
+use function Cli\line;
+use function Cli\prompt;
+
+function sayHello():void {
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+}
+
