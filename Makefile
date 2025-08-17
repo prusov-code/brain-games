a2c737ca67e05872e256fe2b9031e4bc22c29172
prusov-code <prusov-official@yandex.ru>
2025-08-18 00:19:42 +0300

    Add brain-calc game

--- Makefile
+++ Makefile
@@ -8,4 +8,5 @@ brain-games:
 	./bin/brain-games
 brain-even:
 	./bin/brain-even
-
+brain-calc:
+	./bin/brain-calc
---
+++ src/CalcGame.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace BrainGames\CalcGame;
+
+use function BrainGames\Cli\playGame;
+
+function startCalcGame(): void
+{
+    $questions = [];
+    $answers = [];
+    for ($i = 0; $i < 3; $i++) {
+        $number1 = rand(1, 100);
+        $number2 = rand(1, 100);
+        $operationNumber = rand(1, 3);
+        $operation = '';
+        $expressionResult = 0;
+        switch ($operationNumber) {
+            case 1:
+                $operation = '+';
+                $expressionResult = $number1 + $number2;
+                break;
+            case 2:
+                $operation = '-';
+                $expressionResult = $number1 - $number2;
+                break;
+            case 3:
+                $operation = '*';
+                $expressionResult = $number1 * $number2;
+                break;
+        }
+        $expression="{$number1}{$operation}{$number2}";
+        $questions[$i] = $expression ;
+        $answers[$i] = $expressionResult;
+    }
+    playGame($questions, $answers, 'What is the result of the expression?');
+}
