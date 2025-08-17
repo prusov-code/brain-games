1f2da606264f2c8f0cdbd863eb1f6326428a8ddd
prusov-code <prusov-official@yandex.ru>
2025-08-18 00:36:10 +0300

    Add Gcd game

---
+++ src/GcdGame.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace BrainGames\GcdGame;
+
+use function BrainGames\Cli\playGame;
+
+function startGcdGame(): void
+{
+    $questions = [];
+    $answers = [];
+    for ($i = 0; $i < 3; $i++) {
+        $number1 = rand(1, 100);
+        $number2 = rand(1, 100);
+
+        $questions[$i] = "{$number1} {$number2}";
+        $answers[$i] = findGcd($number1, $number2);
+    }
+    playGame($questions, $answers, 'Find the greatest common divisor of given numbers.');
+}
+
+function findGcd(int $number1, int $number2): int
+{
+    while ($number2 !== 0) {
+        $tempNumber2 = $number2;
+        $number2 = $number1 % $number2;
+        $number1 = $tempNumber2;
+    }
+    return $number1;
+}
