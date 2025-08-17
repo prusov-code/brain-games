2576e1771bf62da6c92f2762088dfb8bafb3da25
prusov-code <prusov-official@yandex.ru>
2025-08-17 23:50:26 +0300

    Add Even game

--- Makefile
+++ Makefile
@@ -1,8 +1,11 @@
 install:
 	composer install
-brain-games:
-	./bin/brain-games
 validate:
 	composer validate
 lint:
 	composer exec --verbose phpcs -- --standard=PSR12 src bin
+brain-games:
+	./bin/brain-games
+brain-even:
+	./bin/brain-even
+
---
+++ src/EvenGame.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace BrainGames\EvenGame;
+
+use function BrainGames\Cli\playGame;
+
+function startEvenGame(): void
+{
+    $questions = [];
+    $answers = [];
+    for ($i = 0; $i < 3; $i++) {
+        $questions[$i] = rand(1, 100);
+        $answers[$i] = isEven($questions[$i]) ? 'yes' : 'no';
+    }
+    playGame($questions, $answers);
+}
+
+function isEven(int $number): bool
+{
+    return $number % 2 === 0;
+}
